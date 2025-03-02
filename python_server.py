4d692bc1445f44c30411a57c776e0daa786810cc
wesclark20 <wesclark@udel.edu>
2025-03-01 21:19:00 -0500

    flask server fix

--- python_server.py
+++ python_server.py
@@ -1,9 +1,19 @@
-from flask import Flask, jsonify, request
-from email_gather import emails_main
+from flask import Flask, render_template, request
+from email_gather import emails_main  # Assuming emails_main is in emails_checker.py
 import json
 
 app = Flask(__name__)
 
-@app.route('/<number>', methods=['GET'])
-def scan_emails(number):
-    return json.loads(emails_main(int(number)))
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    results = None
+    if request.method == 'POST':
+        try:
+            num_emails = int(request.form['num_emails'])
+            results = emails_main(num_emails)  # Call phishing detection function
+        except ValueError:
+            results = "Invalid input. Please enter a valid number."
+    return render_template('index.html', results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
